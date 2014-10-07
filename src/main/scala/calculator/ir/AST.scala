d1b3054c646696e17571505170ed20de5d807dbf
Andrew Michaud <andrewjmichaud@gmail.com>
2014-10-06 20:06:14 -0700

    Extend AST to add minus, mult, and div

--- src/main/scala/calculator/ir/AST.scala
+++ src/main/scala/calculator/ir/AST.scala
@@ -4,17 +4,22 @@ package calculator.ir
  * -----------
  * Grammar
  * -----------
- * 
- *                   n ∈ 𝒵 
- * 
+ *
+ *                   n ∈ 𝒵
+ *
  *       e ∈ Expr ::= e + t | e - t | t
  *       t ∈ Term ::= t * f | t / f | f
  *       f ∈ Fact ::= n | ( e )
- *  
+ *
  */
 
 sealed abstract class AST
 sealed abstract class Expr extends AST
+sealed abstract class Term extends Expr
+sealed abstract class Fact extends Term
 
-case class Num(n: Int) extends Expr
-case class Plus(left: Expr, right: Expr) extends Expr
+case class Num(n: Int) extends Fact
+case class Plus(left: Expr, right: Term) extends Expr
+case class Minus(left: Expr, right: Term) extends Expr
+case class Mult(left: Term, right: Fact) extends Term
+case class Div(left: Term, right: Fact) extends Term
