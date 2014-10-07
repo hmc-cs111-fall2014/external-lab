0656dddc6d740bc6034f369909bd07576a5931c5
Andrew Michaud <andrewjmichaud@gmail.com>
2014-10-06 20:07:34 -0700

    add tests for parser for mult, div, minus

--- src/test/scala/calculator/parser/ParserTest.scala
+++ src/test/scala/calculator/parser/ParserTest.scala
@@ -9,37 +9,90 @@ import edu.hmc.langtools._
 class CalcParserTests extends FunSpec with LangParseMatchers[AST] {
 
   override val parser = CalcParser.apply _
-  
+
   describe("A number") {
 
     it("can be a single digit") {
       program("1") should parseAs ( 1 )
     }
-    
+
     it ("can be multiple digits") {
       program("10") should parseAs ( 10 )
       program("121") should parseAs ( 121 )
     }
-    
+
     it ("can be a negative number") {
       program("-10") should parseAs ( -10 )
     }
-    
+
     it ("cannot be floating-point number") {
       program("1.1") should not (parse)
       program(" .3") should not (parse)
     }
 
   }
-  
+
   describe("Addition") {
 
     it("can add two numbers") {
       program("1+1") should parseAs ( 1 |+| 1 )
     }
-    
+
     it("can be chained (and is left-associative)") {
       program("1 + 2 + 100") should parseAs ( (1 |+| 2) |+| 100 )
+    }
+
+  }
+
+  describe("Subtraction") {
+
+    it("can subtract two numbers") {
+      program("1-1") should parseAs ( 1 |-| 1 )
+      program("10-2") should parseAs (10 |-| 2)
+    }
+
+    it("can be chained (and is left-associative)") {
+      program("1 + 2 + 100") should parseAs ( (1 |+| 2) |+| 100 )
+      program("10 - 2 - 1") should parseAs ( (10 |-| 2) |-| 1 )
+    }
+
+    it("can handle negative numbers") {
+      program("1 - 10") should parseAs (1 |-| 10)
+    }
+
+  }
+
+  describe("Multiplication") {
+
+    it("can multiply two numbers") {
+      program("1*1") should parseAs ( 1 |*| 1 )
+      program("10*2") should parseAs (10 |*| 2)
+    }
+
+    it("can be chained (and is left-associative)") {
+      program("1 * 2 * 100") should parseAs ( (1 |*| 2) |*| 100 )
+      program("10 * 2 * 1") should parseAs ( (10 |*| 2) |*| 1 )
+    }
+
+    it("can handle negative numbers") {
+      program("1 * -10") should parseAs (1 |*| -10)
+    }
+  }
+
+  describe("Divistion") {
+
+    it("can divide two numbers") {
+      program("1/1") should parseAs ( 1 |/| 1 )
+      program("10/2") should parseAs (10 |/| 2)
+    }
+
+    it("can be chained (and is left-associative)") {
+      program("100 / 5 / 10") should parseAs ( (100 |/| 5) |/| 10 )
+      program("10 / 2 / 1") should parseAs ( (10 |/| 2) |/| 1 )
+    }
+
+    it("can handle negative numbers") {
+      program("10 / -1") should parseAs (10 |/| -1)
     }
 
   }
