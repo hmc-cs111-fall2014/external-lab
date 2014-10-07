d7b3f8861c68ac621349d6b31f2cf2c375bf3944
Philip Davis <pdavis@hmc.edu>
2014-10-07 00:19:22 -0700

    Lab done

--- src/test/scala/calculator/semantics/SemanticsTest.scala
+++ src/test/scala/calculator/semantics/SemanticsTest.scala
@@ -39,5 +39,65 @@ class NumSemanticsTests extends FunSpec
     }
 
   }
+  
+  describe("Subtraction") {
 
+    it("can subtract one number from another") {
+      program("1-1") should compute (0)
+    }
+
+    it("can be chained (and is left-associative)") {
+      program("100 - 20 - 10") should compute (70)
+    }
+
+    it("can handle negative numbers") {
+      program("1 - -1") should compute (2)
+    }
+
+  }
+  
+  describe("Multiplication") {
+    it("can multiply two number") {
+      program("1*1") should compute(1)
+      program("1*0") should compute(0)
+      program("0*1") should compute (0)
+      program("2*2") should compute (4)
+    }
+    
+    it("can be chained (and is left-associative)") {
+      program("1 * 2 * 3") should compute (6)
+    }
+    
+    it("can handle negative numbers") {
+      program("1 * -1") should compute (-1)
+      program("-1 * 1") should compute (-1)
+      program("-1 * -1") should compute (1)
+    }
+  }
+  
+  describe("Division") {
+    it("can divide one number by another") {
+      program("1/1") should compute(1)
+      program("0/1") should compute (0)
+      program("4/2") should compute (2)
+    }
+    
+    it("can be chained (and is left-associative)") {
+      program("10 / 5 / 2") should compute (1)
+    }
+    
+    it("can handle negative numbers") {
+      program("1 / -1") should compute (-1)
+      program("-1 / 1") should compute (-1)
+      program("-1  / -1") should compute (1)
+    }
+  }
+  
+  describe("Parentheses") {
+    it("should force association") {
+      program("(10*2) / 4") should compute (5)
+      program("10 / (4 - 2)") should compute (5)
+    }
+  }
+    
 }
